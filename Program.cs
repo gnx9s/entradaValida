f57cb6e462d0c9c3bc587a5bd2e23262fba1f97d
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-06-11 01:57:58 -0300

    atividade para entrega concluida

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("-- ENTRADA VÁLIDA OU NÃO --");
+
+
+int valorInserido = 10;
+
+while (valorInserido < 0 || valorInserido > 9)
+{
+    mensagem("", ConsoleColor.White);
+    mensagem("Digite um número entre 1 e 9 (ou 0 para cancelar a operação)...", ConsoleColor.White);
+    valorInserido = int.Parse(Console.ReadLine()!);
+}
+
+if (valorInserido == 0)
+{
+    mensagem("Operação cancelada.", ConsoleColor.Red);
+}
+
+else 
+{
+    mensagem($"O número digitado foi = {valorInserido}", ConsoleColor.Green);
+}
+
+void mensagem(string mensagem, ConsoleColor corMensagem)
+{
+    Console.ForegroundColor = corMensagem;
+    Console.WriteLine(mensagem);
+    Console.ResetColor();
+}
